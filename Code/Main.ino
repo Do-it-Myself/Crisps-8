0a2a8f7bf8809d1dd54b6c76dbbc5d35b93f44dd
Prem <63966578+PremSBhoot@users.noreply.github.com>
2022-11-09 10:36:41 +0000

    Structure of crisps added

--- Code/Main.ino
+++ Code/Main.ino
@@ -15,6 +15,26 @@ class LineFollower{
       return false
     }
   }
+}
 
-  
+class Crisps{
+  private:
+  LineFollower lineFollowers[4];
+
+  public:
+  Crisps (int line1, int line2, int line3, int line4, int motor1, int motor2){
+    lineFollowers[0] = LineFollower(line1);
+    lineFollowers[1] = LineFollower(line1);
+    lineFollowers[2] = LineFollower(line1);
+    lineFollowers[3] = LineFollower(line1);
+
+  boolean* getLineData(){
+    static boolean r[4];
+    for(int i = 0; i<4; i++){
+      r[i] = lineFollowers[i].getLineData();
+    }
+    return r;
+  }
+  }
+
 }
