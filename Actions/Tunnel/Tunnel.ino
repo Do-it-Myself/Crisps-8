1615432a2310776a8f37e3fe10518f8fd1f7979d
ChloeYiu <chloe.yiu.yee.man@gmail.com>
2022-11-10 11:47:51 +0000

    incomplete tunnel

--- Actions/Tunnel/Tunnel.ino
+++ Actions/Tunnel/Tunnel.ino
@@ -1,4 +1,63 @@
 #include <HCSR04.h>
+#include <Adafruit_MotorShield.h>
+
+#define FAST 255
+#define SLOW 155 
+#define WALL_DISTANCE_CM 10
+#define WALL_DISTANCE_TOLERANCE 2
+
+class Motors{
+public:
+  // class field
+  Adafruit_MotorShield MotorShield = Adafruit_MotorShield();
+  Adafruit_DCMotor *Motor;
+
+
+  // constructor
+  Motors(int pin)
+  {
+    Motor = MotorShield.getMotor(pin);
+  }
+
+  // functions
+  void begin()
+  {
+    // Find MotorShield
+    if (!MotorShield.begin())
+    {
+      Serial.println("Could not find Motor Shield. Check wiring.");
+      while (1)
+        ;
+    }
+    Serial.println("Motor Shield found.");
+
+    // Initial configuration - turn on
+    Motor->setSpeed(150);
+    Motor->run(FORWARD);
+    Motor->run(RELEASE);
+
+  }
+
+  void forward(int speed)
+  {
+    // set speed of motor
+    Motor->setSpeed(speed);
+    Motor->run(FORWARD);
+  }
+
+  void backward(int speed)
+  {
+    // set speed of motor
+    Motor->setSpeed(speed);
+    Motor->run(BACKWARD);
+  }
+
+  void stop()
+  {
+    // make motor stop
+    Motor->run(RELEASE);
+  }
+};
 
 class Ultrasound
 {
@@ -29,18 +88,35 @@ public:
 };
 
 Ultrasound ultrasound(2,3);
+Motors motorL(1);
+Motors motorR(2);
 
-void tunnel() {
-  
+void tunnel() { // sensor on the left
+  Serial.println(ultrasound.dist());
+  if (ultrasound.distGreaterThan(WALL_DISTANCE_CM + WALL_DISTANCE_TOLERANCE)) { // too right -> turn left
+    motorL.forward(FAST);
+    motorR.forward(FAST);
+    Serial.println("Left");
+  }
+  else if (!ultrasound.distGreaterThan(WALL_DISTANCE_CM - WALL_DISTANCE_TOLERANCE)) { // too left -> turn right
+    motorL.forward(FAST);
+    motorR.forward(FAST);
+    Serial.println("Right");
+  }
+  else { // within tolerance -> move straight
+    motorL.forward(FAST);
+    motorR.forward(FAST);
+    Serial.println("Straight");
+  }
+  delay(200);
 }
 
-
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  motorL.begin();
+  motorR.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  tunnel();
 }
